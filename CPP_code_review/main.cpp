f25f369e1f14ac9f001aecef3e8c5d068da4eab5
Cheryl Estacio <cherylestacio@gmail.com>
2017-02-22 13:59:56 -0800

    Initial Commit

---
+++ CPP_code_review/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP_code_review
+//
+//  Created by Cheryl Estacio on 2/22/17.
+//  Copyright © 2017 Cheryl Estacio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
